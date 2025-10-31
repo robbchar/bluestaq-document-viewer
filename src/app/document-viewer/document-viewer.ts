88358b6024a9ed477c5d21d540d8db991266a34a
Robb Charnock <robbchar@gmail.com>
2025-10-31 07:39:31 -0700

    basic layout for component

---
+++ src/app/document-viewer/document-viewer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-viewer',
+  imports: [],
+  templateUrl: './document-viewer.html',
+  styleUrl: './document-viewer.css',
+})
+export class DocumentViewer {
+
+}
